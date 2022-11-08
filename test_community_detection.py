39a3c4972d2f3e04f0f884fe8976d9326252ce07
DasniloYT <dasniloyt@hotmail.com>
2022-11-08 15:21:01 +0100

    Ajout des tests (create_network -> is_a_community)

--- test_community_detection.py
+++ test_community_detection.py
@@ -1,1 +1,28 @@
-# Make your tests here
+from community_detection import *
+
+amis = lecture_reseau("files/Communaute1.csv")
+dico_res = create_network(amis)
+
+def test_create_network():
+    assert dico_res == dico_reseau(amis)
+    print("Test 1 passed !")
+
+def test_get_people():
+    assert get_people(dico_res) == personnes_reseau(amis)
+    print("Test 2 passed !")
+
+def test_are_friends():
+    assert are_friends(dico_res, "Barbra", "Cloe")
+    assert not are_friends(dico_res, "Mady", "Illtyd")
+    print("Test 3 passed !")
+
+def test_all_his_friends():
+    assert all_his_friends(dico_res, "Mady", ['Idelle', 'Rufino', 'Vittore', 'Jakob', 'Giedrius', 'Barbra'])
+    assert not all_his_friends(dico_res, "Mady", ['Barbra', 'Kirsa', 'Louis', 'Olavi', 'Placide'])
+    print("Test 4 passed !")
+
+def test_is_a_community():
+    assert is_a_community(dico_res, ['Mady', 'Rufino', 'Vittorio', 'Barbra'])
+    assert is_a_community(dico_res, ['Björn', 'Rufino', 'Mady'])
+    assert not is_a_community(dico_res, ["Cloe", "Idelle", "Mady", "Björn", "Marwa"])
+    print("Test 5 passed !")
