d6923360e0f8ae2a494052bb5cb7392b7cf159c3
DasniloYT <dasniloyt@hotmail.com>
2022-11-13 15:25:32 +0100

    Ficher test complété, vérification nécessaire de Luka

--- test_community_detection.py
+++ test_community_detection.py
@@ -25,8 +25,8 @@ def test_are_friends():
 def test_all_his_friends():
     assert all_his_friends(dico_res_comm1, "Mady", ["Idelle", "Rufino", "Vittore", "Jakob", "Giedrius", "Barbra"])
     assert all_his_friends(dico_res_comm2, "Ilies", ["Séverin", "Petru", "Christ", "Placide"])
-    assert not all_his_friends(dico_res_comm1, "Mady", ["Barbra", "Kirsa", "Louis", "Olavi", "Placide"])
-    assert not all_his_friends(dico_res_comm1, "Ilies", ["Phuong", "Argus", "Cameron", "Mélanie"])
+    assert not all_his_friends(dico_res_comm1, "Vittore", ["Cloe", "Rufino", "Olavi", "Björn", "Mady"])
+    assert not all_his_friends(dico_res_comm1, "Kirsa", ["Cloe", "Rufino", "Olavi", "Björn", "Mady"])
     print("Test 4 passed !")
 
 def test_is_a_community():
@@ -36,14 +36,33 @@ def test_is_a_community():
     assert not is_a_community(dico_res_comm2, ["Illtyd", "Finn", "Placide", "Giedrius", "Stéphanie"])
     print("Test 5 passed !")
 
+def test_find_community():
+    assert find_community(dico_res_comm1, ["Idelle", "Mady", "Idelle", "Teodor"]) == ["Idelle", "Mady"]
+    assert find_community(dico_res_comm1, ["Mady", "Rufino", "Olavi"]) == ["Mady", "Rufino"]
+    assert find_community(dico_res_comm2, ["Shayla", "Évike", "Simo", "Idelle"]) == ["Shayla"]
+    assert find_community(dico_res_comm2, ["Silviu", "Chatzkel", "Shirine", "Gordon"]) == ["Silviu"]
+    print("Test 6 passed !")
+
 def test_order_by_decreasing_popularity():
     assert order_by_decreasing_popularity(dico_res_comm1, ["Mady"]) == ["Mady"]
     assert order_by_decreasing_popularity(dico_res_comm1, ["Giedrius", "Mady", "Kirsa", "Vittore"]) == ["Mady", "Giedrius", "Vittore", "Kirsa"]
     assert order_by_decreasing_popularity(dico_res_comm2, ["Zayneb"]) == ["Zayneb"]
     assert order_by_decreasing_popularity(dico_res_comm2, ["Grwn", "Amadeo", "Cloe", "Pavao"]) == ["Pavao", "Amadeo", "Grwn", "Cloe"]
-    print("Test 6 passed !")
+    print("Test 7 passed !")
 
 def test_find_community_by_decreasing_popularity():
     assert find_community_by_decreasing_popularity(dico_res_comm1) == ["Rufino", "Mady", "Vittorio", "Barbra"]
     assert find_community_by_decreasing_popularity(dico_res_comm2) == ["Dragan", "Cain", "Petru"]
-    print("Test 7 passed !")
+    print("Test 8 passed !")
+
+def test_find_community_from_person():
+    assert find_community_from_person(dico_res_comm1, "Idelle") == ["Idelle", "Mady"]
+    assert find_community_from_person(dico_res_comm1, "Giedrius") == ["Giedrius", "Rufino", "Mady", "Vittorio"]
+    assert find_community_from_person(dico_res_comm2, "Zayneb") == ["Zayneb"]
+    assert find_community_from_person(dico_res_comm2, "Mady") == ["Mady", "Cain", "Petru"]
+    print("Test 9 passed !")
+
+def test_find_max_community():
+    assert find_max_community(dico_res_comm1) == ["Barbra", "Rufino", "Mady", "Vittorio"]
+    assert find_max_community(dico_res_comm2) == ["Mady", "Cain", "Petru"]
+    print("Test 10 passed !")
